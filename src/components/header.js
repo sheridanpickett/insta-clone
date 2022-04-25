8888c10a5c830dc19efa5f03e25e0207f2def274
sheridan <sheridan@sheridans-MacBook-Air.local>
2022-04-25 02:18:46 +0100

    added file upload

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,27 @@
+import { axios } from 'axios'
 import Logo from '../images/logo.png';
 
 export default function Header() {
+
+    const handleClick = async () => {
+        try {
+            const response = await axios.get('http://localhost:4000');
+            console.log(await response.json());
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
     return(
         <header className="border-b border-gray-100 h-16 flex justify-center">
             <div className="max-w-main w-full px-5 flex justify-between items-center">
                 <div className="h-9 flex items-end">
                     <img className="w-24" src={Logo} />
                 </div>
-                <button>
+                <button onClick={handleClick}>
                     <svg aria-label="New post" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24">
-                        <path d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
-                        <line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="6.545" x2="17.455" y1="12.001" y2="12.001"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="12.003" x2="12.003" y1="6.545" y2="17.455"></line>
+                        <path d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
+                        <line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="6.545" x2="17.455" y1="12.001" y2="12.001"></line><line fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" x1="12.003" x2="12.003" y1="6.545" y2="17.455"></line>
                     </svg>
                 </button>
             </div>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,41 @@
+import axios from 'axios';
+import { useState } from 'react';
 import Header from '../components/header';
 
 export default function Dashboard() {
+
+    const [imageFile, setImageFile] = useState(null);
+
+    const handleFileSelect = e => {
+        setImageFile(e.target.files[0]);
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('imageFile', imageFile);
+        try {
+            const res = await axios.post(
+                'http://localhost:4000/images',
+                formData,
+                { headers: {'content-type': 'multipart/form-data'}}
+            )
+            console.log(res);
+        } catch(err) {
+            console.log(err);
+        }
+    }
+    
     return (
         <>
             <Header />
+            <div className="flex flex-col items-center pt-20">
+                <h1>Upload a photo:</h1>
+                <form onSubmit={handleSubmit}>
+                    <input onChange={handleFileSelect} type="file" name="fileupload" accept="image/jpeg,image/png,image/heic,image/heif,video/mp4,video/quicktime" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </>
     )
 }
